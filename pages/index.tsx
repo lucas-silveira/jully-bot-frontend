143a33b59257c27e2a911aa1e47e9bb4cf14c5b1
Lucas Silveira <lugano1995@gmail.com>
2021-01-04 20:07:40 -0300

    feat: add logged validation in index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-  return <h1>Hello World!</h1>
+import { GetStaticProps } from 'next';
+
+export default function Home(): React.ReactNode {
+  return <></>;
 }
+
+export const getStaticProps: GetStaticProps<any> = async () => {
+  const isLogged = false;
+
+  return {
+    redirect: {
+      destination: isLogged ? '/dashboard' : '/login',
+      permanent: false,
+    },
+  };
+};
