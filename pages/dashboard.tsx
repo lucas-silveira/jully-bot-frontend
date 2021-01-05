92a479efbe96d9d6089bc8eb57d8501645cd2b8d
Lucas Silveira <lugano1995@gmail.com>
2021-01-05 15:45:33 -0300

    refactor: dashboard page

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,3 +1,25 @@
+import { useEffect, useState } from 'react';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { CircularProgress } from '@material-ui/core';
+import { Backdrop } from '@styles/components/backdrop.style';
+
 export default function Dashboard(): JSX.Element {
-  return <h1>Dashboard</h1>;
+  const router = useRouter();
+  const [isAuthenticated] = useState(false);
+
+  useEffect(() => {
+    router.push(isAuthenticated ? 'dashboard' : 'login');
+  }, [router, isAuthenticated]);
+  return (
+    <>
+      <Head>
+        <title>Meu dashboard</title>
+      </Head>
+      <Backdrop open>
+        <CircularProgress color="inherit" />
+      </Backdrop>
+      <h1>Dashboard</h1>
+    </>
+  );
 }
