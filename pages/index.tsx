d1dc180ea5328718c3824a225474704cff6182b1
Lucas Silveira <lugano1995@gmail.com>
2021-01-07 11:23:22 -0300

    feat: add header brand into index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import { CircularProgress } from '@material-ui/core';
 import { Backdrop } from '@styles/components/backdrop.style';
+import HeaderBrand from '@components/header/header-brand';
+import * as S from '@styles/pages/index.style';
 
 export default function Home(): JSX.Element {
   return (
@@ -11,6 +13,9 @@ export default function Home(): JSX.Element {
       <Backdrop open>
         <CircularProgress color="inherit" />
       </Backdrop>
+      <S.LayoutWrapper>
+        <HeaderBrand />
+      </S.LayoutWrapper>
     </>
   );
 }
